396e88f28c111ba4addbc418b860ed87f8ef316b
karimElmeniawi <83244907+karimElmeniawi@users.noreply.github.com>
2021-08-07 13:53:29 +0200

    initial commit

---
+++ Safe4LivesProject/Safe4LivesProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Safe4LivesProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
